bc29dada3a48674e25d84732308feae5dde65cd1
Jordan Lambrecht <jordan@pixelbakery.com>
2022-12-04 12:58:46 -0600

    Fixed type classifications, added bg color prop

--- components/modules/Section.tsx
+++ components/modules/Section.tsx
@@ -2,12 +2,16 @@ import cn from 'classnames'
 
 interface Props {
   className?: string
-  children: any
+  children?: any
   id: string
+  bgColor?: string
 }
-const Section = ({ children, id, className }: Props) => {
+const Section = ({ children, id, className, bgColor }: Props) => {
   return (
-    <section className={cn('my-12 py-4', { [`${className}`]: className })} id={id}>
+    <section
+      className={cn('my-12 py-4', { [`${className}`]: className }, { [`bg-${bgColor}`]: bgColor })}
+      id={id}
+    >
       {children}
     </section>
   )
