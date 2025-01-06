087e00cc080eb83e6643d2874743875390d894eb
Jordan Lambrecht <jordan@pixelbakery.com>
2025-01-06 17:02:40 -0600

    feat: added plausible tracking

--- next.config.js
+++ next.config.js
@@ -1,22 +1,23 @@
 /** @type {import('next').NextConfig} */
 
-const React = require("react");
+const React = require('react')
+const { withPlausibleProxy } = require('next-plausible')
 
 const nextConfig = {
   images: {
-    formats: ["image/webp"],
+    formats: ['image/webp'],
     domains: [],
   },
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = withPlausibleProxy({
+  customDomain: 'https://analytics.jordy.world',
+})(nextConfig)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,31 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 import { Footer, Nav } from '@modules/index'
+import PlausibleProvider from 'next-plausible'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const plausibleConfig = {
+    domain: 'nebraska-creative.com',
+    trackOutboundLinks: true,
+    trackLocalhost: process.env.NODE_ENV !== 'production',
+    selfHosted: true,
+    customDomain: 'https://analytics.jordy.world',
+    enabled: true,
+  }
+
   return (
     <>
-      <div className='flex w-full'>
-        <Nav />
-        <div className='w-full px-8 lg:px-4'>
-          <div className='min-h-screen py-12'>
-            <Component {...pageProps} />
+      <PlausibleProvider {...plausibleConfig}>
+        <div className='flex w-full'>
+          <Nav />
+          <div className='w-full px-8 lg:px-4'>
+            <div className='min-h-screen py-12'>
+              <Component {...pageProps} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
         </div>
-      </div>
+      </PlausibleProvider>
     </>
   )
 }
